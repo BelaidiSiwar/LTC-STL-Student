6908572c61ba25010ebdea87bb0023a0f7e7be14
Belaidi <sbelaidi@luxoft.com>
2023-02-10 14:09:13 +0100

    adaptors_task_Belaidi

--- adaptors_task/student/include/impl.h
+++ adaptors_task/student/include/impl.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <string>
+#include <stack>
+
+using namespace std;
 
 /**
  * @todo isValid - check if given expression is valid - all open brackets are closed
@@ -11,4 +14,22 @@
  *
  * @return true if expression is valid
  */
-bool isValid(const std::string& source);
+bool isValid(const std::string& source){
+    stack <string> openingbrackets;
+    for (const auto &ch : source){
+        if (openingbrackets.empty()){
+            openingbrackets.push(&ch);
+        }
+        else if ((openingbrackets.top() == "(" && &ch == ")") || (openingbrackets.top() == "[" && &ch == "]") || (openingbrackets.top() == "{" && &ch == "}")) {
+            openingbrackets.pop();
+        }
+        else {
+            openingbrackets.push(&ch);
+        }
+    }
+    if (openingbrackets.empty()) {
+        return true;
+    }
+    return false;
+
+}
